bd088d6a46f65dc5dc67255e2aebfe3c723c815a
reekotubbs <reekotubbs@hotmail.com>
2023-07-24 07:15:00 -0700

    Update carstate.py
    minimal code change to support 2016 Chevrolet Volt Premium w/o radar using opgm-dev code from @brianbri6

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -4,15 +4,26 @@ from common.numpy_fast import mean
 from opendbc.can.can_define import CANDefine
 from opendbc.can.parser import CANParser
 from selfdrive.car.interfaces import CarStateBase
-from selfdrive.car.gm.values import DBC, CAR, AccState, CanBus, STEER_THRESHOLD
+
+# from opgm-dev add EV_CAR
+from selfdrive.car.gm.values import DBC, CAR, EV_CAR, AccState, CanBus, STEER_THRESHOLD
+#
 
 
 class CarState(CarStateBase):
   def __init__(self, CP):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]["pt"])
-    self.shifter_values = can_define.dv["ECMPRDNL"]["PRNDL"]
+    
+    # from opgm-dev replace ECMPRDNL and PRNDL with ECMPRDNL2 and PRNDL2
+    self.shifter_values = can_define.dv["ECMPRDNL2"]["PRNDL2"]
+    #
+    
     self.lka_steering_cmd_counter = 0
+    
+    # from opgm-dev set park_brake to 0
+    self.park_brake = 0
+    #
 
   def update(self, pt_cp, loopback_cp):
     ret = car.CarState.new_message()
@@ -30,12 +41,42 @@ class CarState(CarStateBase):
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     ret.standstill = ret.vEgoRaw < 0.01
 
-    ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(pt_cp.vl["ECMPRDNL"]["PRNDL"], None))
+    # from opgm-dev
+    gmGear = self.shifter_values.get(pt_cp.vl["ECMPRDNL2"]["PRNDL2"], None)
+    manualMode = bool(pt_cp.vl["ECMPRDNL2"]["ManualMode"])
+    
+    # In manumatic, the value of PRNDL2 represents the max gear
+    # parse_gear_shifter expects "T" for manumatic
+    # TODO: JJS Add manumatic max gear to cereal and parse (validate value in Acadia)
+    if manualMode:
+      gmGear = "T"
+    
+    # TODO: JJS: Should We just have 0 map to P in the dbc?
+    if gmGear == "Shifting":
+      gmGear = "P"
+    
+    ret.gearShifter = self.parse_gear_shifter(gmGear)
+    #
+    
     ret.brakePressed = pt_cp.vl["ECMEngineStatus"]["Brake_Pressed"] != 0
-    ret.brake = pt_cp.vl["EBCMBrakePedalPosition"]["BrakePedalPosition"] / 0xd0
+    
+    # from opgm-dev set brake to 0, then check if brakePressed true
+    ret.brake = 0.
+    
+    if ret.brakePressed:
+    #
+      
+      ret.brake = pt_cp.vl["EBCMBrakePedalPosition"]["BrakePedalPosition"] / 0xd0
 
-    ret.gas = pt_cp.vl["AcceleratorPedal2"]["AcceleratorPedal2"] / 254.
-    ret.gasPressed = ret.gas > 1e-5
+    # from opgm-dev check if enableGasInterceptor true
+    if self.CP.enableGasInterceptor:
+      ret.gas = (pt_cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS"] + pt_cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS2"]) / 2.
+      ret.gasPressed = ret.gas > 15
+    else:
+    #
+      
+      ret.gas = pt_cp.vl["AcceleratorPedal2"]["AcceleratorPedal2"] / 254.
+      ret.gasPressed = ret.gas > 1e-5
 
     ret.steeringAngleDeg = pt_cp.vl["PSCMSteeringAngle"]["SteeringWheelAngle"]
     ret.steeringRateDeg = pt_cp.vl["PSCMSteeringAngle"]["SteeringWheelRate"]
@@ -66,7 +107,11 @@ class CarState(CarStateBase):
     self.pcm_acc_status = pt_cp.vl["AcceleratorPedal2"]["CruiseState"]
 
     # Regen braking is braking
-    if self.car_fingerprint == CAR.VOLT:
+    
+    # from opgm-dev address all electric gm vehicles
+    if self.car_fingerprint in EV_CAR:
+    #
+      
       ret.brakePressed = ret.brakePressed or pt_cp.vl["EBCMRegenPaddle"]["RegenPaddle"] != 0
 
     ret.cruiseState.enabled = self.pcm_acc_status != AccState.OFF
@@ -95,7 +140,12 @@ class CarState(CarStateBase):
       ("FRWheelSpd", "EBCMWheelSpdFront"),
       ("RLWheelSpd", "EBCMWheelSpdRear"),
       ("RRWheelSpd", "EBCMWheelSpdRear"),
-      ("PRNDL", "ECMPRDNL"),
+      
+      # from opgm-dev replace PRNDL and ECMPRDNL with PRNDL2 and ECMPRDNL2 and add ManualMode
+      ("PRNDL2", "ECMPRDNL2"),
+      ("ManualMode", "ECMPRDNL2"),
+      #
+      
       ("LKADriverAppldTrq", "PSCMStatus"),
       ("LKATorqueDelivered", "PSCMStatus"),
       ("LKATorqueDeliveredStatus", "PSCMStatus"),
@@ -107,7 +157,11 @@ class CarState(CarStateBase):
 
     checks = [
       ("BCMTurnSignals", 1),
-      ("ECMPRDNL", 10),
+      
+      # from opgm-dev replace ECMPRDNL with ECMPRDNL2
+      ("ECMPRDNL2", 10),
+      #
+      
       ("PSCMStatus", 10),
       ("ESPStatus", 10),
       ("BCMDoorBeltStatus", 10),
@@ -121,7 +175,22 @@ class CarState(CarStateBase):
       ("EBCMBrakePedalPosition", 100),
     ]
 
-    if CP.carFingerprint == CAR.VOLT:
+    # from opgm-dev include all electric gm vehicles
+    # TODO: Might be wise to find the non-electronic parking brake signal
+    # TODO: JJS Add hasEPB to cereal
+    if CP.carFingerprint != CAR.SUBURBAN and CP.carFingerprint != CAR.TAHOE_NR:
+      signals.append(("EPBClosed", "EPBStatus", 0))
+      checks.append(("EPBStatus", 20))
+    
+
+    if CP.enableGasInterceptor:
+      signals.append(("INTERCEPTOR_GAS", "GAS_SENSOR"))
+      signals.append(("INTERCEPTOR_GAS2", "GAS_SENSOR"))
+      checks.append(("GAS_SENSOR", 50))
+
+    if CP.carFingerprint in EV_CAR:
+    #
+      
       signals.append(("RegenPaddle", "EBCMRegenPaddle"))
       checks.append(("EBCMRegenPaddle", 50))
 
