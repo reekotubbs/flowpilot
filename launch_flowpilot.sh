7a9b6b3af352494608abb9da15a6d32cc21dc3d5
reekotubbs <reekotubbs@hotmail.com>
2023-09-04 17:11:02 -0700

    Direct fingerprint to bolt_euv

--- launch_flowpilot.sh
+++ launch_flowpilot.sh
@@ -15,9 +15,11 @@ export ZMQ_MESSAGING_PROTOCOL="TCP" # TCP, INTER_PROCESS, SHARED_MEMORY
 
 export SIMULATION="1"
 #export FINGERPRINT="HONDA CIVIC 2016"
+export BOLT_EUV = "CHEVROLET BOLT EUV 2022"
+#export VOLT_NR = "CHEVROLET VOLT NO RADAR"
 
 ## android specific ##
-export USE_SNPE="0" # only works for snapdragon devices.
+export USE_SNPE="1" # only works for snapdragon devices.
 
 if pgrep -x "flowinit" > /dev/null
     then
